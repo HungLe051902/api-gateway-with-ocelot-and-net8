e696d2602d5f6a700a0e8b7e17b423227d16f286
HungLe <lexuanhung020599@gmail.com>
2024-02-14 22:12:58 +0700

    Add project files.

---
+++ Product.API/Controllers/LoadBalancerTestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Product.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoadBalancerTestController : ControllerBase
+    {
+        // GET: api/<LoadBalancerTestController>
+        [HttpGet]
+        public string Get()
+        {
+            return "LoadBalancerTest in Product API";
+        }
+
+        // GET api/<LoadBalancerTestController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LoadBalancerTestController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoadBalancerTestController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoadBalancerTestController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
